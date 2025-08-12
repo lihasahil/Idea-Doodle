08a63184cf55017acd5141d19ffbfec38d77ff39
lihasahil <sahilshrestha106@gmail.com>
2025-08-12 18:06:15 +0545

    feature: add new boards functionality

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptySearch } from "./empty-search";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewButtonBoard } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -18,7 +19,22 @@ interface BoardListProps {
 export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = useQuery(api.boards.get, { orgId });
 
-  if (data === undefined) return <div>Loading...</div>;
+  if (data === undefined)
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favourites ? "Favourite Boards" : "Team Boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewButtonBoard orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
 
   if (!data?.length && query.search) return <EmptySearch />;
   if (!data?.length && query.favourites) return <EmptyFavourites />;
@@ -30,6 +46,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         {query.favourites ? "Favourite Boards" : "Team Boards"}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewButtonBoard orgId={orgId} />
         {data?.map((board) => (
           <BoardCard
             key={board._id}
